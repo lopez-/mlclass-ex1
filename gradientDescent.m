72c3b13e1fa41d1e213d7332fe683ad58cf94894
lopez- <victorlopezse@gmail.com>
2015-05-09 21:06:27 +0100

    exercise done

--- gradientDescent.m
+++ gradientDescent.m
@@ -12,21 +12,21 @@ for iter = 1:num_iters
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
     %               theta. 
+	
+	theta = theta - (alpha/m)*(((X*theta)-y)'*X)';
+
     %
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
-
-
     % ============================================================
 
     % Save the cost J in every iteration    
     J_history(iter) = computeCost(X, y, theta);
+
+	Jtemp = sum( ( ( (X*theta)-y).^2 / (2*m) ) )
+	
+	J_history(iter) = Jtemp;
 
 end
 
